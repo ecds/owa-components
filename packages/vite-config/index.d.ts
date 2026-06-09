e0b8d24351a21e31fc8c137ad751d3b1cade9845
Jay Varner <jayvarner@gmail.com>
2026-06-09 11:28:41 -0400

    Add targets

--- packages/vite-config/index.d.ts
+++ packages/vite-config/index.d.ts
@@ -4,6 +4,7 @@ interface WordPressConfigOptions {
   plugins?: Plugin[];
   reactless?: boolean;
   base?: string;
+  target?: string | string[];
 }
 
 export function createWordPressConfig(options?: WordPressConfigOptions): UserConfig;
